96df28b2dd7c7dd231778e2559323df300724be6
SimLeek <josh.miklos@gmail.com>
2018-03-23 16:25:54 -0700

    refactor - removed models other than seif

--- slam2d/SLAM2D.py
+++ slam2d/SLAM2D.py
@@ -56,8 +56,6 @@ if __name__ == '__main__':
         # Basic and EIF estimator for robot state
         mu = state.copy()
         mu[robotFeaturesDim:] += np.random.normal(0, covarianceMeasurements[0, 0], nbLandmark * envFeaturesDim).reshape(nbLandmark * envFeaturesDim, 1)
-        muEKF = mu.copy()
-        muEIF = mu.copy()
         muSEIF = mu.copy()
 
         ## --------------------
@@ -65,32 +63,15 @@ if __name__ == '__main__':
 
         motionModel = BasicMovement(maxSpeed, maxRotation, covarianceMotion, measureFunction)
         measurementModel = BasicMeasurement(covarianceMeasurements, robotFeaturesDim, envFeaturesDim, measureFunction, gradMeasureFunction, detectionSize, detectionCone)
-        ekf = EKFModel(dimension, robotFeaturesDim, envFeaturesDim, motionModel, measurementModel, covarianceMeasurements, mu)
-        eif = EIFModel(dimension, robotFeaturesDim, envFeaturesDim, motionModel, measurementModel, covarianceMeasurements, mu)
         seif = SEIFModel(dimension, robotFeaturesDim, envFeaturesDim, motionModel, measurementModel, covarianceMeasurements, mu, 4)
 
         mus_simple = np.zeros((T, dimension))
-        mus_ekf = np.zeros((T, dimension))
-        mus_eif = np.zeros((T, dimension))
         mus_seif = np.zeros((T, dimension))
         states = np.zeros((T, dimension))
 
         mus_simple[0] = np.squeeze(mu)
-        mus_ekf[0] = np.squeeze(muEKF)
-        mus_eif[0] = np.squeeze(muEIF)
-        mus_seif[0] = np.squeeze(muEIF)
         states[0] = np.squeeze(state)
 
-
-        # LOG Initial state
-        print("BEFORE")
-        print("EIF estimate :")
-        print(muEIF)
-        print("EKF estimate :")
-        print(muEKF)
-        print("Real state :")
-        print(state)
-        print('\n')
 
         for t in range(1, T):
             print("\nIteration %d" % t)
@@ -99,51 +80,18 @@ if __name__ == '__main__':
 
             mu += motionModel.exact_move(mu, motionCommand)
 
-            H, _ = ekf.update(measures, landmarkIds, motionCommand, covarianceMotion)
-            print (H != 0).sum(), ' / ', H.size
-            H, _= eif.update(measures, landmarkIds, motionCommand, covarianceMotion)
-            print (H != 0).sum(), ' / ', H.size
             H, _, _ = seif.update(measures, landmarkIds, motionCommand, covarianceMotion)
             print (H != 0).sum(), ' / ', H.size
 
-            muEKF = ekf.estimate()
-            muEIF = eif.estimate()
             muSEIF = seif.estimate()
 
-            print "np.linalg.norm(muEIF-muSEIF)"
-            print np.linalg.norm(muEIF-muSEIF)
-            print np.linalg.norm(eif.b - seif.b)
-            print np.linalg.norm(eif.H - seif.H)
-            print muEIF[:3]
-            print muSEIF[:3]
+            print(muSEIF[:3])
 
 
             mus_simple[t] = np.squeeze(mu)
-            mus_ekf[t] = np.squeeze(muEKF)
-            mus_eif[t] = np.squeeze(muEIF)
             mus_seif[t] = np.squeeze(muSEIF)
             states[t] = np.squeeze(state)
 
-
-        # LOG Final state
-        print('\n')
-        print('AFTER')
-        print("EIF estimate :")
-        print(muEIF)
-        print("EKF estimate :")
-        print(muEKF)
-        print("Real state :")
-        print(state)
-        print("Final Error EIF:")
-        print(state - muEIF)
-        print("Final Error EKF:")
-        print(state - muEKF)
-        print("Final Max Error EIF: %f" % max(state-muEIF))
-        print("Final Norm Error EIF: %f" % np.linalg.norm(state-muEIF))
-        print("Final Max Error EKF: %f" % max(state-muEKF))
-        print("Final Norm Error EKF: %f" % np.linalg.norm(state-muEKF))
-        print("Final Max Error SEIF: %f" % max(state-muSEIF))
-        print("Final Norm Error SEIF: %f" % np.linalg.norm(state-muSEIF))
 
         landmarks = state[robotFeaturesDim:].reshape(nbLandmark, 2)
         plt.figure()
@@ -156,8 +104,6 @@ if __name__ == '__main__':
 
         plt.plot(states[:, 0], states[:, 1])
         plt.plot(mus_simple[:, 0], mus_simple[:, 1])
-        plt.plot(mus_ekf[:, 0], mus_ekf[:, 1])
-        plt.plot(mus_eif[:, 0], mus_eif[:, 1])
         plt.plot(mus_seif[:, 0], mus_seif[:, 1])
 
         plt.legend(['Real position', 'Simple estimate', 'EKF estimate', 'EIF estimate', 'SEIF estimate'])
